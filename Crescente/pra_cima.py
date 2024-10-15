fcfe3c9390e351366f1f870a4b151960f8f4852b
Vatreax <rafasmontiel13@gmail.com>
2024-10-15 09:17:09 -0400

    pra cima

--- Crescente/pra_cima.py
+++ Crescente/pra_cima.py
@@ -1,8 +1,10 @@
-lista = [].sorted()
+lista = []
 n1 = int(input("Quantos números deseja inserir: "))
 
 contador = 0
 while contador < n1:
     contador += 1
-    n2 = int(input(f"Digite o {contador}Número: "))
+    n2 = int(input(f"Digite o {contador}°Número: "))
     lista.append(n2)
+
+print(sorted(lista))
