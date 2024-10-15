ad105d3b52771301ec510659cb5d71cb5672b4ec
Vatreax <rafasmontiel13@gmail.com>
2024-10-15 09:30:36 -0400

    Correções 2

--- Crescente/pra_cima.py
+++ Crescente/pra_cima.py
@@ -3,8 +3,11 @@ n1 = int(input("Quantos números deseja inserir: "))
 
 contador = 0
 while contador < n1:
-    contador += 1
-    n2 = int(input(f"Digite o {contador}°Número: "))
-    lista.append(n2)
+    try:
+        n2 = int(input(f"Digite o {contador+1}°Número Inteiro: "))
+        lista.append(n2)
+        contador += 1
+    except ValueError:
+        print("\nDigite um Número Inteiro!\n")
 
 print(sorted(lista))
